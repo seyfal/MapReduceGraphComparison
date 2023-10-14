b8d09fd245ed79c59273f4f63f30ec6c4fe482b6
Seyfal Sultanov <seyfal.s@outlook.com>
2023-10-13 23:50:34 -0500

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,76 +1,110 @@
-import Explanations.explainResults
-import GoldenSetLoader.{parseFile, extractList, extractMapKeys}
-import GraphSharder.{divideGraph, shuffleGraph, storeGraphInHDFS, writeShardsToHDFS_nodes}
-import OutputFileLoader.loadOutputFile
+package com.lsc
+
+import com.lsc.Explanations.explainResults
+import com.lsc.GoldenSetLoader.parseFile
+import com.lsc.GraphSharder.{divideGraph, shuffleGraph, storeGraphInHDFS, writeShardsToHDFS_nodes}
+import com.lsc.OutputFileLoader.loadOutputFile
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.mapreduce.Job
-import org.slf4j.LoggerFactory
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.Text
+import org.apache.hadoop.mapreduce.Job
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
+import org.slf4j.LoggerFactory
 
 import java.net.URI
 import java.text.SimpleDateFormat
 import java.util.Date
 
+/** Entry point for the similarity computation based on MapReduce.
+  * This program divides the original and perturbed graphs into pieces, shuffles them,
+  * and then runs a MapReduce job to calculate similarity scores. It then computes
+  * various metrics based on the similarity scores and golden set.
+  */
 object Main {
-  private val logger = LoggerFactory.getLogger(this.getClass)
-  private val methodName = "Main"
   private type Node = Int
   private type Graph = Map[Node, Set[Node]]
   private type GraphPieces = List[Graph]
+  private val logger = LoggerFactory.getLogger(this.getClass)
+  private val methodName = "Main"
 
   def main(args: Array[String]): Unit = {
     try {
-      
+
       logger.info(s"$methodName: Starting the program")
-      
-      logger.info(s"$methodName: Loading the configuration file")
-      
+
+      // Configuration loading
+      logger.info(s"$methodName: Loading configurations")
+
       logger.info(s"$methodName: Loading the original graph")
       val originalGraphFilePath = ConfigurationLoader.getOriginalGraphFilePath
       logger.info(s"$methodName: Loading the perturbed graph")
       val perturbedGraphFilePath = ConfigurationLoader.getPerturbedGraphFilePath
-      logger.info(s"$methodName: Loading the shard folder")
-      val shardFolder = ConfigurationLoader.getShardFolder
       logger.info(s"$methodName: Loading the number of pieces")
       val numPieces = ConfigurationLoader.getNumPieces
       logger.info(s"$methodName: Loading the golden set path")
       val goldenSetPath = ConfigurationLoader.getGoldenSetFilePath
 
+      // Graph division
+      logger.info(s"$methodName: Dividing the graphs into pieces")
+
       logger.info(s"$methodName: Dividing the original graph into pieces")
       val originalGraph = GraphLoader.loadGraph(originalGraphFilePath)
       logger.info(s"$methodName: Dividing the perturbed graph into pieces")
       val perturbedGraph = GraphLoader.loadGraph(perturbedGraphFilePath)
-      
+
       // TODO: Update the output directory
 
-      logger.info(s"$methodName: Dividing the original graph into $numPieces pieces")
-      val originalPieces = divideGraph(originalGraph, numPieces, outputDir = shardFolder, false)
-      logger.info(s"$methodName: Dividing the perturbed graph into $numPieces pieces")
-      val perturbedPieces = divideGraph(perturbedGraph, numPieces, outputDir = shardFolder, false)
+      logger.info(
+        s"$methodName: Dividing the original graph into $numPieces pieces"
+      )
+      val originalPieces =
+        divideGraph(originalGraph, numPieces)
+      logger.info(
+        s"$methodName: Dividing the perturbed graph into $numPieces pieces"
+      )
+      val perturbedPieces =
+        divideGraph(perturbedGraph, numPieces)
 
+      // Graph shuffling
       logger.info(s"$methodName: Shuffling the original graph")
       val shuffledGraph = shuffleGraph(originalPieces, perturbedPieces)
 
-      logger.info(s"$methodName: Upload the shards to the HDFS and save the directory path")
+      // Storing in HDFS
+      logger.info(s"$methodName: Uploading graphs and shards to HDFS")
+
+      logger.info(
+        s"$methodName: Upload the shards to the HDFS and save the directory path"
+      )
       val hdfsDirectoryPath = writeShardsToHDFS_nodes(shuffledGraph)
-      logger.info(s"$methodName: The shards are uploaded to the HDFS and the directory path is $hdfsDirectoryPath")
+      logger.info(
+        s"$methodName: The shards are uploaded to the HDFS and the directory path is $hdfsDirectoryPath"
+      )
+
+      logger.info(s"$methodName: Loading the hdfs directory path")
+      val hdfsUserDirectory = ConfigurationLoader.getUserDirectory
+      logger.info(s"$methodName: Loading the original graph path in HDFS")
+      val originalGraphPath = ConfigurationLoader.getOriginalGraphPath
+      logger.info(s"$methodName: Loading the perturbed graph path in HDFS")
+      val perturbedGraphPath = ConfigurationLoader.getPerturbedGraphPath
+      logger.info(s"$methodName: Loading the hdfs base")
+      val hdfsBase = ConfigurationLoader.getHdfsBase
+      logger.info(s"$methodName: Loading the job jar path")
+      val jobJarPath = ConfigurationLoader.getJobPath
 
       logger.info(s"$methodName: Upload the original graph to the HDFS")
-      storeGraphInHDFS(originalGraph, "/user/seyfal/CS441/originalGraph.txt")
+      storeGraphInHDFS(originalGraph, originalGraphPath)
 
       logger.info(s"$methodName: Upload the perturbed graph to the HDFS")
-      storeGraphInHDFS(perturbedGraph, "/user/seyfal/CS441/perturbedGraph.txt")
+      storeGraphInHDFS(perturbedGraph, perturbedGraphPath)
 
       logger.info(s"$methodName: Configuring the MapReduce job")
       val job = Job.getInstance(new Configuration(), "NodeSimilarityJob")
-      job.setJar("/Users/seyfal/Desktop/CS441 Cloud/SimRank/SimRank/target/scala-3.3.1/simrank_3-0.1.0-SNAPSHOT.jar")
+      job.setJar(jobJarPath)
 
       logger.info(s"$methodName: Adding the original and perturbed graphs to the cache")
-      job.addCacheFile(new URI("hdfs://localhost:9000/user/seyfal/CS441/originalGraph.txt"))
-      job.addCacheFile(new URI("hdfs://localhost:9000/user/seyfal/CS441/perturbedGraph.txt"))
+      job.addCacheFile(new URI(s"$hdfsBase$originalGraphPath"))
+      job.addCacheFile(new URI(s"$hdfsBase$perturbedGraphPath"))
 
       // Specify Mapper (and optionally Reducer) class
       job.setMapperClass(classOf[SimilarityMapper])
@@ -82,11 +116,16 @@ object Main {
 
       // Input and Output formats
       logger.info(s"$methodName: Setting the input and output formats")
-      FileInputFormat.addInputPath(job, new Path(s"hdfs://localhost:9000$hdfsDirectoryPath"))
+      FileInputFormat.addInputPath(
+        job,
+        new Path(s"$hdfsBase$hdfsDirectoryPath")
+      )
       logger.info(s"$methodName: Setting the output format")
 
-      val outputTimestamp = new SimpleDateFormat("HH.mm.ss_dd.MM.yyyy").format(new Date())
-      val outputPath = s"hdfs://localhost:9000/user/seyfal/CS441//output_$outputTimestamp"
+      val outputTimestamp =
+        new SimpleDateFormat("HH.mm.ss_dd.MM.yyyy").format(new Date())
+      val outputPath =
+        s"$hdfsBase$hdfsUserDirectory/output_$outputTimestamp"
       FileOutputFormat.setOutputPath(job, new Path(outputPath))
 
       // Run the job
@@ -99,13 +138,19 @@ object Main {
 
       // load your reducer's output
       val outputData = loadOutputFile(s"$outputPath/part-r-00000")
-      logger.info(s"$methodName: Output file is loaded and the size is ${outputData.size}")
+      logger.info(
+        s"$methodName: Output file is loaded and the size is ${outputData.size}"
+      )
 
-      val (modifiedNodes, removedNodes, addedNodeKeys) = parseFile(goldenSetPath)
+      val (modifiedNodes, removedNodes, addedNodeKeys) = parseFile(
+        goldenSetPath
+      )
 
       val nodesFromOutput = outputData.keys.map(_._1).toSet
-      val mySameNodes = nodesFromOutput.filter(node => outputData((node, node)) == 1.0)
-      val myModifiedNodes = nodesFromOutput.filter(node => outputData((node, node)) < 1.0)
+      val mySameNodes =
+        nodesFromOutput.filter(node => outputData((node, node)) == 1.0)
+      val myModifiedNodes =
+        nodesFromOutput.filter(node => outputData((node, node)) < 1.0)
 
       println(s"Modified nodes: $modifiedNodes")
       println(s"Removed nodes: $removedNodes")
@@ -116,10 +161,14 @@ object Main {
       // Compute GTL, BTL, ATL, DTL, CTL, WTL
 
       // Compute CTL
-      val CTL = modifiedNodes.count(node => !mySameNodes.contains(node) && !addedNodeKeys.contains(node))
+      val CTL = modifiedNodes.count(node =>
+        !mySameNodes.contains(node) && !addedNodeKeys.contains(node)
+      )
 
       // Compute WTL
-      val WTL = myModifiedNodes.count(node => !modifiedNodes.contains(node) && !addedNodeKeys.contains(node))
+      val WTL = myModifiedNodes.count(node =>
+        !modifiedNodes.contains(node) && !addedNodeKeys.contains(node)
+      )
 
       // Compute DTL
       val DTL = removedNodes.count(node => !nodesFromOutput.contains(node))
@@ -152,5 +201,5 @@ object Main {
         logger.error(s"Error occurred in Main: ", e)
     }
   }
-  
+
 }
