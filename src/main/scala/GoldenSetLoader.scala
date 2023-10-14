ca2de12d50841bffd93d31840666180eb89f3ae0
Seyfal Sultanov <seyfal.s@outlook.com>
2023-10-13 23:49:35 -0500

    Update GoldenSetLoader.scala

--- src/main/scala/GoldenSetLoader.scala
+++ src/main/scala/GoldenSetLoader.scala
@@ -1,16 +1,19 @@
+package com.lsc
+
 import scala.io.Source
 import scala.util.matching.Regex
 
+/**
+ * Object responsible for loading a golden set of modifications from a file.
+ */
 object GoldenSetLoader {
 
-  def main(args: Array[String]): Unit = {
-    val (modifiedNodes, removedNodes, addedNodeKeys) = parseFile("path_to_your_file.txt")
-
-    println(s"Modified Nodes: $modifiedNodes")
-    println(s"Removed Nodes: $removedNodes")
-    println(s"Added Node Keys: $addedNodeKeys")
-  }
-
+  /**
+   * Parse a file to extract the modified, removed, and added node keys.
+   *
+   * @param filePath The path to the file containing the golden set data.
+   * @return A tuple containing sets of modified, removed, and added node keys.
+   */
   def parseFile(filePath: String): (Set[Int], Set[Int], Set[Int]) = {
     val fileContent = Source.fromFile(filePath).mkString
 
@@ -21,6 +24,14 @@ object GoldenSetLoader {
     (modifiedNodes, removedNodes, addedNodeKeys)
   }
 
+  /**
+   * Extract a list of integers from a specific section and category of the content.
+   *
+   * @param section The section name within the content.
+   * @param category The category name within the section.
+   * @param content The string content to extract the list from.
+   * @return A set of integers extracted from the specified section and category.
+   */
   private def extractList(section: String, category: String, content: String): Set[Int] = {
     val pattern = new Regex(s"(?s)$section.*?$category\\s*\\[([^\\]]+)\\]")
     pattern.findFirstMatchIn(content) match {
@@ -29,6 +40,14 @@ object GoldenSetLoader {
     }
   }
 
+  /**
+   * Extract map keys from a specific section and category of the content.
+   *
+   * @param section The section name within the content.
+   * @param category The category name within the section.
+   * @param content The string content to extract the map keys from.
+   * @return A set of map keys (integers) extracted from the specified section and category.
+   */
   private def extractMapKeys(section: String, category: String, content: String): Set[Int] = {
     val pattern = new Regex(s"(?s)$section.*?$category((?:\\s*[0-9]+:\\s*[0-9]+\\s*)+)")
     pattern.findFirstMatchIn(content) match {
